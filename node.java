397d225c66ad1e5e8d3b7777cee4791f346b48a4
justhussh <100328132+justhussh@users.noreply.github.com>
2022-05-27 15:50:58 +0700

    Add files via upload

---
+++ node.java
@@ -0,0 +1,11 @@
+public class node {
+    int nomor;
+    String nama;
+    node prev, next;
+    node(node prev, int nomor, String nama, node next){
+        this.prev = prev;
+        this.nomor = nomor;
+        this.nama = nama;
+        this.next = next;
+    }
+}
